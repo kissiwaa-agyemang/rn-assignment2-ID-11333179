753a4cb2fd93f0d8bab6b27ddfce3dcc8f1af29e
kissiwaa-agyemang <150914504+kissiwaa-agyemang@users.noreply.github.com>
2024-05-27 08:46:19 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My name is <Text style={{fontWeight:'bold'}}>Ewurama</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
